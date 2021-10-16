66752ebd2d23ec82cc68f8b7c0e85d8bca16db21
Gastonviga <87142425+Gastonviga@users.noreply.github.com>
2021-10-16 16:05:16 -0300

    Create ej4.c

---
+++ ej4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int total(int fuerza, int superficie){
+	int presion;
+	presion=fuerza/superficie;
+	return presion;
+}
+int main(){
+	int a,b;
+	printf("ingrese la fuerza: ");
+	scanf("%d",&a);
+	printf("ingrese la superficie: ");
+	scanf("%d",&b);
+	
+	int res=total(a,b);
+	printf("el resultado es: %d",res);
+}
